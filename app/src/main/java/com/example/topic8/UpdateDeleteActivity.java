417ba04c01f0d1b0599c6253063cfee37f0d7f54
Ashish <ashishDongol32@gmail.com>
2019-12-25 10:44:30 +0545

    not complete

--- app/src/main/java/com/example/topic8/UpdateDeleteActivity.java
+++ app/src/main/java/com/example/topic8/UpdateDeleteActivity.java
@@ -1,7 +1,9 @@
 package com.example.topic8;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -49,7 +51,26 @@ public class UpdateDeleteActivity extends AppCompatActivity {
         btnDel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                deleteEmployee();
+                AlertDialog.Builder builder = new AlertDialog.Builder(UpdateDeleteActivity.this);
+                builder.setMessage("Are you sure you want to delete this employee ?");
+                builder.setTitle("Alert !");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteEmployee();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
             }
         });
 
